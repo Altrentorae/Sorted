42ae558feac6dadab1198da98a0346fcf6f68226
JP <killer39800@gmail.com>
2019-10-04 01:41:08 +0100

    Initial commit

---
+++ Sorted/UI.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Sorted {
+    partial class UI {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listBox_vArray = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_GenerateNew = new System.Windows.Forms.Button();
+            this.button_Sort = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_passes = new System.Windows.Forms.Label();
+            this.checkBox_AutoPasses = new System.Windows.Forms.CheckBox();
+            this.comboBox_SortMethod = new System.Windows.Forms.ComboBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.label_time = new System.Windows.Forms.Label();
+            this.textBox_seedInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox_vArray
+            // 
+            this.listBox_vArray.FormattingEnabled = true;
+            this.listBox_vArray.Location = new System.Drawing.Point(12, 12);
+            this.listBox_vArray.Name = "listBox_vArray";
+            this.listBox_vArray.Size = new System.Drawing.Size(120, 524);
+            this.listBox_vArray.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(138, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1084, 524);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // button_GenerateNew
+            // 
+            this.button_GenerateNew.Location = new System.Drawing.Point(1123, 542);
+            this.button_GenerateNew.Name = "button_GenerateNew";
+            this.button_GenerateNew.Size = new System.Drawing.Size(99, 38);
+            this.button_GenerateNew.TabIndex = 2;
+            this.button_GenerateNew.Text = "New";
+            this.button_GenerateNew.UseVisualStyleBackColor = true;
+            this.button_GenerateNew.Click += new System.EventHandler(this.button_GenerateNew_Click);
+            // 
+            // button_Sort
+            // 
+            this.button_Sort.Enabled = false;
+            this.button_Sort.Location = new System.Drawing.Point(1023, 542);
+            this.button_Sort.Name = "button_Sort";
+            this.button_Sort.Size = new System.Drawing.Size(94, 38);
+            this.button_Sort.TabIndex = 3;
+            this.button_Sort.Text = "Sort";
+            this.button_Sort.UseVisualStyleBackColor = true;
+            this.button_Sort.Click += new System.EventHandler(this.button_Sort_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(666, 541);
+            this.trackBar1.Maximum = 2000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(157, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 538);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Passes";
+            // 
+            // label_passes
+            // 
+            this.label_passes.AutoSize = true;
+            this.label_passes.Location = new System.Drawing.Point(619, 554);
+            this.label_passes.Name = "label_passes";
+            this.label_passes.Size = new System.Drawing.Size(37, 13);
+            this.label_passes.TabIndex = 6;
+            this.label_passes.Text = "AUTO";
+            // 
+            // checkBox_AutoPasses
+            // 
+            this.checkBox_AutoPasses.AutoSize = true;
+            this.checkBox_AutoPasses.Checked = true;
+            this.checkBox_AutoPasses.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_AutoPasses.Location = new System.Drawing.Point(619, 570);
+            this.checkBox_AutoPasses.Name = "checkBox_AutoPasses";
+            this.checkBox_AutoPasses.Size = new System.Drawing.Size(48, 17);
+            this.checkBox_AutoPasses.TabIndex = 7;
+            this.checkBox_AutoPasses.Text = "Auto";
+            this.checkBox_AutoPasses.UseVisualStyleBackColor = true;
+            this.checkBox_AutoPasses.CheckedChanged += new System.EventHandler(this.checkBox_AutoPasses_CheckedChanged);
+            // 
+            // comboBox_SortMethod
+            // 
+            this.comboBox_SortMethod.FormattingEnabled = true;
+            this.comboBox_SortMethod.Items.AddRange(new object[] {
+            "BubbleSort",
+            "InsertSort",
+            "BogoSort",
+            "CountingSort"});
+            this.comboBox_SortMethod.Location = new System.Drawing.Point(892, 564);
+            this.comboBox_SortMethod.Name = "comboBox_SortMethod";
+            this.comboBox_SortMethod.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_SortMethod.TabIndex = 8;
+            this.comboBox_SortMethod.SelectedIndexChanged += new System.EventHandler(this.comboBox_SortMethod_SelectedIndexChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(456, 542);
+            this.trackBar2.Maximum = 2000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(157, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackbar_arrSize);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 538);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Size";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(419, 555);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(31, 13);
+            this.sizeLabel.TabIndex = 6;
+            this.sizeLabel.Text = "1000";
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Location = new System.Drawing.Point(12, 556);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(33, 13);
+            this.label_time.TabIndex = 9;
+            this.label_time.Text = "TIME";
+            // 
+            // textBox_seedInput
+            // 
+            this.textBox_seedInput.Location = new System.Drawing.Point(892, 538);
+            this.textBox_seedInput.Name = "textBox_seedInput";
+            this.textBox_seedInput.Size = new System.Drawing.Size(121, 20);
+            this.textBox_seedInput.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(859, 542);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Seed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(841, 567);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Algorithm";
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 589);
+            this.Controls.Add(this.textBox_seedInput);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.comboBox_SortMethod);
+            this.Controls.Add(this.checkBox_AutoPasses);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.label_passes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button_Sort);
+            this.Controls.Add(this.button_GenerateNew);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox_vArray);
+            this.Name = "UI";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_vArray;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_GenerateNew;
+        private System.Windows.Forms.Button button_Sort;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_passes;
+        private System.Windows.Forms.CheckBox checkBox_AutoPasses;
+        private System.Windows.Forms.ComboBox comboBox_SortMethod;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.TextBox textBox_seedInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Sorted/UI.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorted {
+    public partial class UI : Form {
+        public UI() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox_SortMethod.SelectedIndex = 0;
+            trackBar2.Value = 1000;
+        }
+
+        int[] Unsorted;
+        private void button_GenerateNew_Click(object sender, EventArgs e) {
+            Unsorted = new int[arrSize];
+            Tuple<int, int> GBounds = new Tuple<int, int>(0, 1000);
+
+            button_Sort.Enabled = true;
+            Random RNG = new Random(textBox_seedInput.Text == "" ? DateTime.Now.Millisecond : textBox_seedInput.Text.GetHashCode());
+            for (int i = 0; i < Unsorted.Length; i++) {
+                Unsorted[i] = RNG.Next(GBounds.Item1, GBounds.Item2);
+            }
+            BindToUI(Unsorted);
+        }
+
+        private Color ColorFromValue(Color target, int value, int upperBound = 100) {
+            decimal pct = (decimal)value / (decimal)upperBound;
+            int R, G, B;
+            R = (int)(target.R * pct);
+            G = (int)(target.G * pct);
+            B = (int)(target.B * pct);
+            return Color.FromArgb(R,G,B);
+            
+        }
+
+        private void BindToUI(int[] source) {
+            
+            //Chart
+            chart1.Series.Clear();
+            chart1.Series.Add("Values");
+            for(int i=0; i < source.Length; i++) {
+                chart1.Series["Values"].Points.AddXY(i,source[i]);
+                chart1.Series["Values"].Points[i].Color = ColorFromValue(Color.Red, source[i], 1000);
+            }
+            chart1.Update();
+
+            //ListBox
+            listBox_vArray.DataSource = null;
+            listBox_vArray.Items.Clear();
+            listBox_vArray.DataSource = source;
+            listBox_vArray.Update();
+        }
+        
+        private void InterfaceControlsEnabled(bool Enabled) {
+            
+            button_GenerateNew.Enabled = Enabled;
+            button_Sort.Enabled = Enabled;
+            trackBar1.Enabled = Enabled;
+            trackBar2.Enabled = Enabled;
+            textBox_seedInput.Enabled = Enabled;
+            comboBox_SortMethod.Enabled = Enabled;
+        }
+        
+        private async void button_Sort_Click(object sender, EventArgs e) {
+            //await Task.Run(() => Sort());
+            Sort();
+        }
+
+        private void Sort() {
+            InterfaceControlsEnabled(false);
+
+            //First switch is just to get passes under automatic pass count
+            if (checkBox_AutoPasses.Checked) {
+                switch (SM) {
+                    case SortMethod.BubbleSort: passes = Unsorted.Length; break;
+                    case SortMethod.InsertSort:
+                    case SortMethod.BogoSort:
+                    case SortMethod.CountingSort: passes = 1; break;
+                }
+                trackBar1.Value = passes;
+            }
+            DateTime dt = DateTime.Now;
+
+            for (int i = 0; i < passes; i++) {
+                switch (SM) {
+                    case SortMethod.BubbleSort:
+
+                    Unsorted = BubbleSort(Unsorted);
+                    BindToUI(Unsorted); break; //Bind to UI is only supported for predictable sorts, all others are handled in-method
+                    case SortMethod.InsertSort:
+                    Unsorted = InsertSort(Unsorted, chart1);
+                    //BindToUI(Unsorted); 
+                    break;
+                    case SortMethod.BogoSort:
+                    if (checkBox_AutoPasses.Checked) {
+                        while (!IsSorted(Unsorted)) {
+                            Unsorted = BogoSort(Unsorted);
+                            BindToUI(Unsorted);
+                            chart1.Update();
+                        }
+                    }
+                    else {
+                        if (!IsSorted(Unsorted)) {
+                            Unsorted = BogoSort(Unsorted);
+                        }
+                    }
+                    break;
+                    case SortMethod.CountingSort:
+                    CountingSort(Unsorted);
+                    break;
+                }
+            }
+
+            label_time.Text = (DateTime.Now - dt).ToString();
+            InterfaceControlsEnabled(true);
+        }
+        
+
+        private enum SortMethod : byte {
+            BubbleSort = 0,
+            InsertSort = 1,
+            BogoSort = 2,
+            CountingSort = 3
+        }
+
+        int passes = 0;
+        private void trackBar1_Scroll(object sender, EventArgs e) {
+            passes = trackBar1.Value;
+            label_passes.Text = trackBar1.Value.ToString();
+        }
+
+        
+        private void checkBox_AutoPasses_CheckedChanged(object sender, EventArgs e) {
+            trackBar1.Enabled = !checkBox_AutoPasses.Checked;
+            label_passes.Text = checkBox_AutoPasses.Checked ? "AUTO" : trackBar1.Value.ToString();
+        }
+
+        SortMethod SM;
+        private void comboBox_SortMethod_SelectedIndexChanged(object sender, EventArgs e) {
+            SM = (SortMethod)comboBox_SortMethod.SelectedIndex;
+        }
+
+        int arrSize = 1000;
+        private void trackbar_arrSize(object sender, EventArgs e) {
+            sizeLabel.Text = trackBar2.Value.ToString();
+            arrSize = trackBar2.Value;
+        }
+
+        private bool IsSorted(int[] source) {
+            for(int i = 0; i < source.Length; i++) {
+                for(int j = 0; j < source.Length; j++) {
+                    if(source[i] > source[j] && i < j) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static int[] BubbleSort(int[] source) {
+            int temp = 0;
+            for (int i = 0; i < source.Length - 1; i++) {
+
+                if (source[i] > source[i + 1]) {
+                    temp = source[i];
+                    source[i] = source[i + 1];
+                    source[i + 1] = temp;
+                }
+            }
+            return source;
+        }
+
+        public int[] InsertSort(int[] source, Control Chart = null) {
+            List<int> Li = new List<int>(source);
+            for (int i = 0; i < Li.Count-1; i++) {
+                for(int j = 1; j<Li.Count; j++) {
+                    if(Li[j] < Li[i] && i < j) {
+                        Li.Insert(i, Li[j]);
+                        Li.RemoveAt(j+1);
+
+                        if(Chart != null) { 
+                            Chart.Update();
+                            BindToUI(Li.ToArray());
+                        }
+                    }
+                }
+            }
+            return Li.ToArray();
+        }
+
+        public int[] BogoSort(int[] source) {
+            int temp = 0;
+            int r = 0;
+            Random RNG = new Random();
+            for(int i = 0; i < source.Length-1; i++) {
+                r = RNG.Next(0, source.Length);
+                temp = source[i];
+                source[i] = source[r];
+                source[r] = temp;
+            }
+            return source;
+        }
+
+        public int[] CountingSort(int[] source) {
+            Dictionary<int, int> vps = new Dictionary<int, int>();
+            List<int> @new = new List<int>();
+            foreach(int i in source) {
+                if (!vps.ContainsKey(i)) {
+                    vps.Add(i, 1);
+                }
+                else {
+                    vps[i]++;
+                }
+            }
+
+            int c = source.Min();
+            while(@new.Count!=source.Length) {
+                if (vps.ContainsKey(c)) {
+                    for(int j = 0; j < vps[c]; j++) {
+                        @new.Add(c);
+                        BindToUI(@new.ToArray());
+                        chart1.Update();
+                    }
+                    
+                }
+                c++;
+            }
+            return @new.ToArray();
+        }
+    }
+}
